b6b8ac2f9440ae48a4342f9b0c3564d6508ddc06
Brooke Milberg <147758777+rh-35@users.noreply.github.com>
2024-05-22 20:53:09 +0000

    mobile theme switcher

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -18,102 +18,118 @@ import {
     DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
 import { Button } from "@/components/ui/button"
-import { ChevronRight } from "lucide-react"
+import { ChevronRight, MoonIcon, SunIcon, SunMoonIcon } from "lucide-react"
 import { SunMoon } from 'lucide-react';
 import { useTheme } from "next-themes"
 
 export function Navbar() {
-    const { setTheme } = useTheme()
+    const { setTheme, theme } = useTheme()
+    function toggleTheme() {
+        if (theme == "dark") {
+            setTheme("light")
+        }
+        if (theme == "light") {
+            setTheme("system")
+        }
+        if (theme == "system") {
+            setTheme("dark")
+        }
+    }
     return (
         <div className="w-full fixed left-0 top-0 z-40">
             <div className="hidden sm:block">
                 <div className="h-4 bg-white dark:bg-zinc-900" />
             </div>
             <div className="mx-0 sm:mx-16 xl:mx-48">
-            <nav className="mx-auto flex bg-zinc-100/70 dark:bg-zinc-800/70 h-16 w-full backdrop-blur-md items-center justify-between space-x-3 sm:rounded-full px-4">
-                <div>
-                    <Link
-                        href="/"
-                        className="flex items-center space-x-3 rtl:space-x-reverse"
-                    >
-                        <Image
-                            className=" border border-red-600 rounded-full hover:animate-spin h-9 w-9"
-                            src="/images/icon.png"
-                            width="250"
-                            height="250"
-                            alt="Icon"
-                            quality={100}
-                        />
-                        <span className="hover:text-red-600 transition duration-250 text-center text-2xl font-semibold">
-                            Brooke Milberg
-                        </span>
-                    </Link>
-                </div>
-                <div className="hidden sm:block">
-                    <div className="justify-right text-xl flex items-center font-semibold space-x-3">
+                <nav className="mx-auto flex bg-zinc-100/70 dark:bg-zinc-800/70 h-16 w-full backdrop-blur-md items-center justify-between space-x-3 sm:rounded-full px-4">
+                    <div>
                         <Link
-                            href="/resume"
-                            className="hover:text-red-600 transition duration-250"
+                            href="/"
+                            className="flex items-center space-x-3 rtl:space-x-reverse"
                         >
-                            Resume
+                            <Image
+                                className=" border border-red-600 rounded-full hover:animate-spin h-9 w-9"
+                                src="/images/icon.png"
+                                width="250"
+                                height="250"
+                                alt="Icon"
+                                quality={100}
+                            />
+                            <span className="hover:text-red-600 transition duration-250 text-center text-2xl font-semibold">
+                                Brooke Milberg
+                            </span>
                         </Link>
-                        <Link
-                            href="/showcase"
-                            className="hover:text-red-600 transition duration-250"
-                        >
-                            Showcase
-                        </Link>
-                        <ThemeSwitcher />
                     </div>
-                </div>
-                <div className="sm:hidden">
-                    <DropdownMenu>
-                        <DropdownMenuTrigger asChild>
-                            <Button variant="outline" size="icon" className="rounded-full dark:bg-zinc-900">
-                                <ChevronRight className="h-6 w-6" />
-                            </Button>
-                        </DropdownMenuTrigger>
-                        <DropdownMenuContent className="w-auto">
-                            <DropdownMenuItem>
-                                <Link
-                                    href="/resume"
-                                    className="hover:text-red-600 transition duration-250"
-                                >
-                                    Resume
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuItem>
-                                <Link
-                                    href="/showcase"
-                                    className="hover:text-red-600 transition duration-250"
-                                >
-                                    Showcase
-                                </Link>
-                            </DropdownMenuItem>
-                            <DropdownMenuSeparator />
-                                <DropdownMenuSub>
-                                    <DropdownMenuSubTrigger>
-                                        <SunMoon className="h-6 w-6 mr-2" />
-                                        <span>Switch Theme</span>
-                                    </DropdownMenuSubTrigger>
-                                    <DropdownMenuPortal>
-                                        <DropdownMenuSubContent>
-                                            <DropdownMenuItem onClick={() => setTheme("light")}>
-                                                Light
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem onClick={() => setTheme("dark")}>
-                                                Dark
-                                            </DropdownMenuItem>
-                                            <DropdownMenuItem onClick={() => setTheme("system")}>
-                                                System
-                                            </DropdownMenuItem>
-                                        </DropdownMenuSubContent>
-                                    </DropdownMenuPortal>
-                                </DropdownMenuSub>
-                        </DropdownMenuContent>
-                    </DropdownMenu>
-                </div>
-            </nav>
+                    <div className="hidden sm:block">
+                        <div className="justify-right text-xl flex items-center font-semibold space-x-3">
+                            <Link
+                                href="/resume"
+                                className="hover:text-red-600 transition duration-250"
+                            >
+                                Resume
+                            </Link>
+                            <Link
+                                href="/showcase"
+                                className="hover:text-red-600 transition duration-250"
+                            >
+                                Showcase
+                            </Link>
+                            <ThemeSwitcher />
+                        </div>
+                    </div>
+                    <div className="sm:hidden">
+                        <DropdownMenu>
+                            <DropdownMenuTrigger asChild>
+                                <Button variant="outline" size="icon" className="rounded-full dark:bg-zinc-900">
+                                    <ChevronRight className="h-6 w-6" />
+                                </Button>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-auto">
+                                <DropdownMenuItem>
+                                    <Link
+                                        href="/resume"
+                                        className="hover:text-red-600 transition duration-250"
+                                    >
+                                        Resume
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuItem>
+                                    <Link
+                                        href="/showcase"
+                                        className="hover:text-red-600 transition duration-250"
+                                    >
+                                        Showcase
+                                    </Link>
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem onClick={() => toggleTheme()} >
+                                    {theme === "dark" ? (
+                                        <>
+                                            <MoonIcon className="mr-3" />
+                                            Dark
+                                        </>
+                                    ) : (
+                                        <>
+                                            {
+                                                theme === "light" ? (
+                                                    <>
+                                                        <SunIcon className="mr-3" />
+                                                        Light
+                                                    </>
+                                                ) : (
+                                                    <>
+                                                        <SunMoonIcon className="mr-3" />
+                                                        System
+                                                    </>
+                                                )
+                                            }
+                                        </>
+                                    )}
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
+                    </div>
+                </nav>
             </div>
         </div>
     );
