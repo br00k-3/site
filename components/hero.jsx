2bd9be0475120dfcbb04e734036a427eac5abda1
Brooke Milberg <147758777+br00k-3@users.noreply.github.com>
2024-05-30 20:50:19 +0000

    new hero

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,13 +1,17 @@
 "use client"
 import Lottie from '@lottielab/lottie-player/react';
+import Link from "next/link";
 export function Hero() {
     return (
         <div className="flex-col text-center w-full">
-            <div className="flex text-6xl h-16 font-bold text-center w-full space-x-4">
-                <div className='flex justify-between items-center w-full space-x-2'>
-                    <span>A sound tech</span>
+            <div className="flex text-6xl font-bold text-center w-full space-x-2">
+                <div className='flex justify-between items-center'>
+                    A sound tech, with a passion
+                </div>
+                <div>
+                    <Link href="/">
                     <Lottie className="h-14" src="https://cdn.lottielab.com/l/AmAm8tW1DFV4sg.json" autoplay />
-                <span>, with a passion</span>
+                    </Link>
                 </div>
                
             </div>
