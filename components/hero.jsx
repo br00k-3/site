9196fc46e8a18b0478f5ad7147b03d6e2c0296e2
rh-35 <rheamilberg@gmail.com>
2024-06-01 22:08:25 -0700

    removed comma on hero

--- components/hero.jsx
+++ components/hero.jsx
@@ -1,18 +1,17 @@
-"use client"
+"use client";
 import Lottie from '@lottielab/lottie-player/react';
-import Link from "next/link";
 export function Hero() {
-    return (
-        <div className="flex-col text-center w-full">
-            <div className="text-6xl font-bold text-center w-full">
-                <span>A sound tech, with a passion </span>
-                <span className='inline-block'><Link href="/">
-                    <Lottie className="h-14" src="https://cdn.lottielab.com/l/AmAm8tW1DFV4sg.json" autoplay />
-                    </Link></span>      
-            </div>
-            <div className="text-2xl text-zinc-400 font-semibold">
-                Theatre technician based in Portland Oregon
-            </div>
-        </div>
-    )
+  return (
+    <div className="flex-col text-center w-full">
+      <div className="text-6xl font-bold text-center w-full">
+        <span>A sound tech with a passion </span>
+        <span className="inline-block">
+          <Lottie className="h-14" src="https://cdn.lottielab.com/l/AmAm8tW1DFV4sg.json" autoplay />
+        </span>
+      </div>
+      <div className="text-2xl text-zinc-400 font-semibold">
+        Theatre technician based in Portland Oregon
+      </div>
+    </div>
+  );
 }
