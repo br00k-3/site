2eda92f9f63a4a96b7ad50ee5dafdbdc4bddfa11
rh-35 <rheamilberg@gmail.com>
2024-06-12 22:39:10 -0700

    fixed nav centering

--- app/listening-stats/page.jsx
+++ app/listening-stats/page.jsx
@@ -4,7 +4,7 @@ import ListeningTrends from "@/components/listening-trends";
 
 export default function page() {
   return (
-    <div className="xl:mx-56 md:mx-20 sm:mx-8 mx-4">
+    <div>
       <p className="text-3xl font-semibold">Music Stats</p>
       <p className="text-zinc-400">Data taken from Last.fm</p>
       <div className="w-full mt-1 md:w-auto">
--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,19 @@
-import { Hero } from "@/components/hero"
-import { Liveskills } from "@/components/liveskills"
-import { Studioskills } from "@/components/studioskills"
+import { Hero } from "@/components/hero";
+import { Liveskills } from "@/components/liveskills";
+import { Studioskills } from "@/components/studioskills";
 
 export default function Home() {
   return (
-    <div className="xl:mx-56 md:mx-20 sm:mx-8 mx-4">
-                  <Hero/>
-
-        <div className="text-center flex h-16 w-full text-2xl font-semibold justify-center items-center">
-          Live Skills
-        </div>
-        <Liveskills/>
-        <div className="text-center flex h-16 w-full text-2xl font-semibold justify-center items-center">
-          Studio Skills
-        </div>
-        <Studioskills/>
+    <div>
+      <Hero />
+      <div className="text-center flex h-16 w-full text-2xl font-semibold justify-center items-center">
+        Live Skills
+      </div>
+      <Liveskills />
+      <div className="text-center flex h-16 w-full text-2xl font-semibold justify-center items-center">
+        Studio Skills
+      </div>
+      <Studioskills />
     </div>
   );
 }
