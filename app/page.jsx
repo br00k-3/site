3f73a1e4f8f25ac5d8f46fab7a7fb50b015a37d6
rh-35 <rheamilberg@gmail.com>
2024-06-15 16:04:39 -0700

    fixed hero spacing (AGAIN)

--- app/page.jsx
+++ app/page.jsx
@@ -5,6 +5,7 @@ import { Studioskills } from "@/components/studioskills";
 export default function Home() {
   return (
     <div>
+      <div className="hidden sm:block h-7"/>
       <Hero />
       <div className="text-center flex h-16 w-full text-2xl font-semibold justify-center items-center">
         Live Skills
