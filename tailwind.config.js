60c0616545861df7e0c44bb9a9e658e0e4fe664d
rh-35 <rheamilberg@gmail.com>
2024-06-20 21:50:01 -0700

    added a bit of blur

--- tailwind.config.js
+++ tailwind.config.js
@@ -164,7 +164,7 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      blur: {
+      backdropBlur: {
         xs: '.5px',
       },
     },
