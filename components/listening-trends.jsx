bc803dc393f1df86583767551a8e3ecde2df1777
Br00k-3 <brookemilberg@gmail.com>
2024-07-31 16:43:33 -0700

    better data visualization

--- components/listening-trends.jsx
+++ components/listening-trends.jsx
@@ -1,14 +1,24 @@
 "use client";
 import {
-  BarChart,
-  CartesianGrid,
-  XAxis,
-  YAxis,
-  Tooltip,
-  ResponsiveContainer,
-  Bar,
-} from "recharts";
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart"
 import { useState, useEffect } from "react";
+
+import { Bar, BarChart, CartesianGrid, LabelList, XAxis, YAxis } from "recharts"
+
+const chartConfig = {
+  playcount: {
+    label: "playcount",
+    color: "#c026d3",
+  },
+  label: {
+    color: "#06b6d4",
+  },
+}
+
 
 export default function ListeningTrends() {
   const [artistData, setArtistData] = useState();
@@ -31,34 +41,56 @@ export default function ListeningTrends() {
       });
   }, []); // empty array at the end would be used to add dependencies, and, if they're changed, react will rerun this code when the values of the dpendencies change.
 
-  const CustomTooltip = ({ active, payload, label }) => {
-    if (active && payload && payload.length) {
-      return (
-        <div className="custom-tooltip bg-zinc-200/90 dark:bg-zinc-900/90 p-2 rounded-lg">
-          <p className="label">{`${label} : ${payload[0].value}`}</p>
-        </div>
-      );
-    }
-
-    return null;
-  };
 
   return (
     <div className="flex-col flex-wrap items-center justify-center">
-      <div>The top 10 artists I listen to</div>
-      <ResponsiveContainer width="100%" aspect={2.5}>
-        <BarChart width={1000} height={1000} data={artistData}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name"/>
-          <YAxis className="text-xs" width={25} />
-          <Tooltip content={<CustomTooltip />} cursor={false} />
-          <Bar
-            name="name"
-            dataKey="playcount"
-            className="fill-fuchsia-600 hover:fill-fuchsia-700"
-          />
-        </BarChart>
-      </ResponsiveContainer>
+      <div className="">The top 10 artists I listen to</div>
+      <ChartContainer config={chartConfig} className="min-h-[500px] w-full">
+          <BarChart
+            accessibilityLayer
+            data={artistData}
+            layout="vertical"
+            margin={{
+              right: 16,
+            }}
+          >
+            <YAxis
+              dataKey="name"
+              type="category"
+              tickLine={false}
+              tickMargin={10}
+              axisLine={false}
+              tickFormatter={(value) => value.slice(0, 3)}
+              hide
+            />
+            <XAxis dataKey="playcount" type="number" hide />
+            <ChartTooltip
+              cursor={false}
+              content={<ChartTooltipContent indicator="line" />}
+            />
+            <Bar
+              dataKey="playcount"
+              layout="vertical"
+              fill="var(--color-playcount)"
+              radius={4}
+            >
+              <LabelList
+                dataKey="name"
+                position="insideLeft"
+                offset={8}
+                className="fill-[--color-label] text-xs lg:text-lg"
+                
+              />
+              <LabelList
+                dataKey="playcount"
+                position="right"
+                offset={8}
+                className="fill-[--color-label]"
+                fontSize={12}
+              />
+            </Bar>
+          </BarChart>
+        </ChartContainer>
     </div>
   );
 }
