50694d0989676bb4f7c35b24de03f844eb6a2116
rh-35 <rheamilberg@gmail.com>
2024-08-02 15:04:51 -0700

    fixed hero lottie

--- components/hero.jsx
+++ components/hero.jsx
@@ -3,13 +3,13 @@ import Lottie from '@lottielab/lottie-player/react';
 export function Hero() {
   return (
     <div className="flex-col text-center w-full light:not-italic dark:not-italic retro:italic">
-      <div className="text-6xl font-bold text-center w-full">
-        <h1>A sound tech with a passion </h1>
+      <h1 className="text-6xl font-bold text-center w-full">
+        <span>A sound tech with a passion </span>
         <span className="inline-block">
           <Lottie className="light:block dark:block retro:hidden h-14" src="https://cdn.lottielab.com/l/AmAm8tW1DFV4sg.json" autoplay />
           <Lottie className="light:hidden dark:hidden retro:block h-14" src="https://cdn.lottielab.com/l/7dXZ4zDhj9uLS2.json" autoplay />
         </span>
-      </div>
+      </h1>
       <h2 className="text-2xl light:text-zinc-400 dark:text-zinc-400 retro:text-green-500 font-semibold">
         Theatre Technician Based In Portland, Oregon
       </h2>
