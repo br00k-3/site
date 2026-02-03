ed403fbecf3141f4aa1a184a89242d68488ac103
Br00k-3 <brookemilberg@gmail.com>
2026-02-02 22:43:45 -0800

    changed certs to be single col on mobile

--- components/home/certifications.jsx
+++ components/home/certifications.jsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export function Certifications() {
   return (
-    <div className="grid relative w-full grid-flow-row gap-10 text-center md:gap-4 md:text-left lg:mb-0 grid-cols-2 md:grid-cols-3">
+    <div className="grid relative w-full grid-flow-row gap-10 text-center md:gap-4 md:text-left lg:mb-0 grid-cols-1 sm:grid-cols-2 md:grid-cols-3">
       {certifications.map((certificationItem) => (
         <div
           key={certificationItem.title}
           className="relative group light:rounded-lg dark:rounded-3xl text-white"
         >
-          <div className="h-32 lg:h-64 w-full overflow-hidden light:rounded-3xl dark:rounded-3xl group-hover:md:scale-125 group-hover:z-20 light:bg-zinc-200 dark:bg-zinc-900 retro:bg-fuchsia-950/70 duration-300 relative place-content-center light:border-white dark:border-zinc-200/70 group-hover:border">
+          <div className="h-42 sm:h-32 lg:h-64 w-full overflow-hidden light:rounded-3xl dark:rounded-3xl group-hover:md:scale-125 group-hover:z-20 light:bg-zinc-200 dark:bg-zinc-900 retro:bg-fuchsia-950/70 duration-300 relative place-content-center light:border-white dark:border-zinc-200/70 group-hover:border">
             <Link href={`${certificationItem.link}`}>
             <div className="w-full h-full absolute top-0 left-0 group-hover:bg-black/20 z-10"/>
               <Image
